e38ef54a97e9fe8189fb3c118071f82146992085
IppolitovIS <IppolitovIS@gmail.com>
2023-10-01 20:35:57 +0300

    Initial commit

---
+++ ShapeAreaCalc/base/Shape.cs
@@ -0,0 +1,13 @@
+﻿namespace ShapeAreaCalc;
+
+public abstract class Shape
+{
+    private readonly double _area;
+
+    protected Shape(Func<double> areaCalculator)
+    {
+        this._area = areaCalculator();
+    }
+
+    public double GetArea => _area;
+}
---
+++ tests/ShapeAreaCalc.Test/AreaTests.cs
@@ -0,0 +1,9 @@
+namespace ShapeAreaCalc.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void circle_area_calculated()
+    {
+    }
+}
